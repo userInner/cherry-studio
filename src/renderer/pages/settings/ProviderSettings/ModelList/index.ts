315a0ac1e59c44c394362fb01b4a9830e86c6bfc
gujiaming <52187003+AtomsH4@users.noreply.github.com>
2026-08-15 22:22:25 +0800

    fix(model-check): complete translations and trim exports
    Signed-off-by: gujiaming <52187003+AtomsH4@users.noreply.github.com>

--- src/renderer/pages/settings/ProviderSettings/ModelList/index.ts
+++ src/renderer/pages/settings/ProviderSettings/ModelList/index.ts
@@ -1,8 +1,2 @@
-export { checkModelWithMultipleKeys } from './checkModelsHealth'
 export { default as ModelList } from './ModelList'
-export {
-  ModelListHealthProvider,
-  useModelListHealth,
-  useModelListHealthResults,
-  useModelListHealthRun
-} from './modelListHealthContext'
+export { ModelListHealthProvider, useModelListHealth } from './modelListHealthContext'
